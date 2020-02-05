472dcaa9a33b0311047480d9acb47f069b55e2c6
Dominic <dtorres243@west-mec.org>
2020-02-05 16:58:56 -0700

    finished case 2

--- case2/hg_report.js
+++ case2/hg_report.js
@@ -12,6 +12,35 @@
 	
 */
 //step 5
-var gameReport = "<h1>" + itemTitle + "</h1> <h2>By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt=" + itemID + "id='gameImg' /> <table> <tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr> <tr><th>Condition</th><td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr> </table>" + itemSummary + "";
+var gameReport = "<h1>" + itemTitle + "</h1> <h2>By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt=" + itemID + "id='gameImg' /> <table> <tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr> <tr><th>Condition</th><td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr> </table>" + itemSummary;
 //step 6
-document.getElementsByTagName().innerHTML
+document.getElementsByTagName("article"[0]).innerHTML = gameReport;
+//step 7
+var ratingsSum = 0;
+//step 8
+var ratingsCount = ratings.length;
+ 
+//step 9
+for(var i = 0; i < ratings.length; i++){
+  ratingsSum + ratings;
+}
+//step 10
+var ratingAvg = ratingsSum / ratingsCount;
+ 
+//step 11
+var ratingReport = "<h1> Customer Review</h1> <h2>" + ratingAvg + " out of 5 stars (" + ratingsCount + " reviews) </h2>";
+ 
+//step 12
+for(var i = 0; i < 3; i++){
+  ratingReport += "<div class='review'> <h1>" + ratingTitles[i] + "</h1> <table><tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr> <tr><th>Rating</th><td>";
+ 
+  for(var j = 0; j < ratings[i]; j++){
+     ratingReport += "<img src='hg_star.png' />";
+  }
+ 
+  //puts ratings in table,
+  ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
+}
+ 
+//step 13
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
