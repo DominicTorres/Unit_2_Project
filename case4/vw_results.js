c4e14cfe7119646af6366f297ea9efc3f6b0e5fb
Dominic <dtorres243@west-mec.org>
2020-02-06 17:13:19 -0700

    finished case 4

--- case4/vw_results.js
+++ case4/vw_results.js
@@ -24,22 +24,54 @@
       
 */
 //step 5, finds the race title
-var reportHTML = "<h1>title</h1>" + raceTitle;
-//step 6,
-loop:
-for(var totalVotes = 0; i < totalVotes; i++){
-   totalVotes.forEach(calcSum);
-   reportHTML = "<table><caption>" + race. + "</caption><tr><th>Candidate</th><th>Votes</th></tr>"
- }
-
-
-/* Callback Function to calculate an array sum */
+var reportHTML = "<h1>" + raceTitle + "</h1>";
+//step 6, loop for race array
+for(var i = 0; i < race.length; i++){
+   var totalVotes = 0;
+   votes[i].forEach(calcSum);
+   reportHTML += "<table> <caption>" + race[i] + "</caption> <tr><th>Candidate</th><th>Votes</th><tr>";
+   reportHTML += candidateRows(i, totalVotes);
+   reportHTML += "</table>"
+}
+//step 7
+document.getElementsByTagName("section")[0].innerHTML = reportHTML;
+//step 8, creates its own individual rows for the each cadidate
+function candidateRows(raceNum, totalVotes){
+   var rowHTML = "";
+   for(var j = 0; j < 3; j++){
+      var candidateName = candidate[raceNum][j];
+      var candidateParty = party[raceNum][j];
+      var candidateVotes = votes[raceNum][j];
+      var candidatePercent = calcPercent(candidateVotes, totalVotes);
+      rowHTML += "<tr><td>" + candidateName + "(" + candidateParty + ")</td> <td>" + candidateVotes.toLocaleString() + "(" + candidatePercent.toFixed(1) + ")</td> "
+      //adds more to bar
+      for(var k = 0; k < candidatePercent; k++){
+         rowHTML += createBar(candidateParty, candidatePercent);
+      }
+      rowHTML+="</tr>";
+   }
+   return rowHTML;
+}
+//step 8 continued
 function calcSum(value) {
    totalVotes += value;
 }
-
-/* Function to calculate a percentage */
+//step 8 continued, finds the percent
 function calcPercent(value, sum) {
    return (100*value/sum);
+}
+//step 10, bar graph that displays the percentage
+function createBar(partyType){
+   var barHTML = "";
+   if(partyType === "D"){
+      barHTML = "<td class = 'dem'></td>"
+   }
+   if(partyType === "R"){
+      barHTML = "<td class = 'rep'></td>"
+   }
+   if(partyType === "I"){
+      barHTML = "<td class = 'ind'></td>"
+   }
+   return barHTML;
 }
 
